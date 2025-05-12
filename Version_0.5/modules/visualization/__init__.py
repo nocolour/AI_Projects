cd2bfc2e42a35d40e429d668ce9dfbaac62dc864
nocolour <nocolour@gmail.com>
2025-05-12 15:50:52 +0800

    Delete Version_0.5 directory

--- Version_0.5/modules/visualization/__init__.py
+++
@@ -1,4 +0,0 @@
-# This file makes the directory a Python package
-from .manager import VisualizationManager
-
-__all__ = ['VisualizationManager']
--- Version_0.5/modules/visualization_manager.py
+++
@@ -1,4 +0,0 @@
-from .visualization.manager import VisualizationManager
-
-# Re-export the VisualizationManager class
-__all__ = ['VisualizationManager']
